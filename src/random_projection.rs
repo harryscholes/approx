e7d2ad6a025d6a224b9c5658ac33207ab05555a9
harryscholes <harryscholes@users.noreply.github.com>
2023-06-05 20:15:25 +0300

    Update API

--- src/random_projection.rs
+++ src/random_projection.rs
@@ -6,9 +6,11 @@ use ndarray_rand::rand_distr::Uniform;
 use ndarray_rand::RandomExt;
 use num::Float;
 
-#[derive(Clone)]
+type Buckets<T> = HashMap<Vec<bool>, Vec<Vec<T>>>;
+
 pub struct RandomProjection<T> {
     plane_norms: Array2<T>,
+    buckets: Buckets<T>,
 }
 
 impl<T> RandomProjection<T>
@@ -16,16 +18,17 @@ where
     T: Float + LinalgScalar + SampleUniform,
 {
     pub fn new(dims: usize, bits: usize) -> Self {
-        Self {
-            plane_norms: Array2::random(
-                (bits, dims),
-                Uniform::new(T::from(-0.5).unwrap(), T::from(0.5).unwrap()),
-            ),
-        }
+        Self::from(Array2::random(
+            (bits, dims),
+            Uniform::new(T::from(-0.5).unwrap(), T::from(0.5).unwrap()),
+        ))
     }
 
     pub fn from(plane_norms: Array2<T>) -> Self {
-        Self { plane_norms }
+        Self {
+            plane_norms,
+            buckets: HashMap::new(),
+        }
     }
 
     pub fn project(&self, vec: &[T]) -> Vec<T> {
@@ -39,34 +42,26 @@ where
             .collect()
     }
 
-    pub fn buckets(&self) -> RandomProjectionBuckets<T> {
-        RandomProjectionBuckets::new(self)
-    }
-}
-
-pub struct RandomProjectionBuckets<T> {
-    rp: RandomProjection<T>,
-    buckets: HashMap<Vec<bool>, Vec<Vec<T>>>,
-}
-
-impl<T> RandomProjectionBuckets<T>
-where
-    T: Float + LinalgScalar + SampleUniform,
-{
-    pub fn new(rp: &RandomProjection<T>) -> Self {
-        Self {
-            rp: rp.clone(),
-            buckets: HashMap::new(),
-        }
+    pub fn hash_to_bucket(&mut self, vec: &[T]) -> Vec<bool> {
+        let hash = self.hash(vec);
+        self.insert_into_bucket(hash.clone(), vec.to_vec());
+        hash
     }
 
-    pub fn insert(&mut self, vec: &[T]) {
-        let h = self.rp.hash(vec);
-        self.buckets.entry(h).or_insert(vec![]).push(vec.to_vec());
+    fn insert_into_bucket(&mut self, hash: Vec<bool>, vec: Vec<T>) {
+        self.buckets.entry(hash).or_insert(vec![]).push(vec);
     }
 
-    pub fn get(&self, k: &[bool]) -> Option<&Vec<Vec<T>>> {
-        self.buckets.get(k)
+    pub fn get_bucket(&self, hash: &[bool]) -> Option<&Vec<Vec<T>>> {
+        self.buckets.get(hash)
+    }
+
+    pub fn nearest_neighbours(&self, vec: &[T]) -> Option<&Vec<Vec<T>>> {
+        self.get_bucket(&self.hash(vec))
+    }
+
+    pub fn buckets(&self) -> &Buckets<T> {
+        &self.buckets
     }
 }
 
@@ -90,13 +85,12 @@ mod tests {
             [0.34768572, -0.37184437],
             [-0.11170635, -0.0242341],
         ]);
-        let rp = RandomProjection::from(plane_norms);
+
+        let mut rp = RandomProjection::from(plane_norms);
 
         let a = vec![1., 2.];
         let b = vec![2., 1.];
         let c = vec![3., 1.];
-
-        let data = vec![a.clone(), b.clone(), c.clone()];
 
         assert_eq!(
             rp.project(&a),
@@ -106,20 +100,37 @@ mod tests {
         assert_eq!(rp.hash(&b), vec![false, true, true, false]);
         assert_eq!(rp.hash(&c), vec![false, true, true, false]);
 
-        let mut rp_buckets = rp.buckets();
-        for vec in &data {
-            rp_buckets.insert(vec);
-        }
+        rp.hash_to_bucket(&a);
+        rp.hash_to_bucket(&b);
+        rp.hash_to_bucket(&c);
 
         assert_eq!(
-            rp_buckets.get(&vec![true, false, false, false]),
+            rp.get_bucket(&vec![true, false, false, false]),
             Some(&vec![a])
         );
         assert_eq!(
-            rp_buckets.get(&vec![false, true, true, false]),
-            Some(&vec![b, c])
+            rp.get_bucket(&vec![false, true, true, false]),
+            Some(&vec![b.clone(), c.clone()])
         );
-        assert!(rp_buckets.get(&vec![true, false, false, true]).is_none());
+        assert!(rp.get_bucket(&vec![true, false, false, true]).is_none());
+
+        assert_eq!(rp.nearest_neighbours(&vec![2.5, 1.]), Some(&vec![b, c]));
+
+        let query = vec![true, true, false, false];
+        let buckets = rp.buckets();
+        let keys: Vec<&Vec<bool>> = buckets.keys().collect();
+        let mut distances: Vec<(&Vec<bool>, usize)> = keys
+            .iter()
+            .map(|bucket| (*bucket, hamming_distance(bucket, &query)))
+            .collect();
+        distances.sort_by(|a, b| a.1.cmp(&b.1));
+        assert_eq!(
+            distances,
+            vec![
+                (&vec![true, false, false, false], 1),
+                (&vec![false, true, true, false], 2),
+            ]
+        );
     }
 
     #[test]
